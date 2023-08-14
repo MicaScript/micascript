bfb139fbe34e88fd1272fd12c08fce65681c2320
Kağan Ege <79661973+tavuk-doner@users.noreply.github.com>
2023-08-14 21:19:52 +0300

    14.08.23

--- src/runtime/utils/FileSystem.js
+++ src/runtime/utils/FileSystem.js
@@ -1,3 +1,17 @@
-function readFile(path, options) {
-  Deno.core.
+function readFile(path) {
+  return Deno.core.ops.op_read_file(path);
 }
+
+function writeFile(path, contents) {
+  return Deno.core.ops.op_write_file(path, contents);
+}
+
+function removeFile(path) {
+  return Deno.core.ops.op_remove_file(path);
+}
+
+function listdir(dir) {
+  return Deno.core.ops.op_read_dir(dir);
+}
+
+// TODO: Add other file transactions.
